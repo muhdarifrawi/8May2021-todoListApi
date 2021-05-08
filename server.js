7096df566652dad28df4159ef72ad8c0cc2c5167
arif <muhdarifrawi@gmail.com>
2021-05-08 21:46:17 +0800

    Connected all via server.js - models, routes and controllers are put together in server.js.

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 var express = require("express"),
     app = express(),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    mongoose = require("mongoose"),
+    // created model
+    Task = require("./api/models/todoListModel"),
+    bodyParser = require("body-parser");
 
+// mongooose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/Tododb");
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+var routes = require("./api/routes/todoListRoutes");
+routes(app);
+
+// listens and displays port 
 app.listen(port);
-
 console.log("todo list RESTful API server started on: " + port );
